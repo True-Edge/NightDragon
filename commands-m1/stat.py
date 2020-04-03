095e91fc02f95731c4597ab520fa26a596ca6bec
DMNight6 <arnokong365@gmail.com>
2020-04-03 09:49:23 +0100

    Rasp Configration 2.

--- commands-m1/stat.py
+++ commands-m1/stat.py
@@ -1,4 +1,4 @@
-@bot.event
+zz@bot.event
 async def on_ready():
     print('----------\nLogged As [{0.user}]\nUser ID: [{0.user.id}]\n----------'.format(bot))
-    await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=2, name="n$ ./[True Edge]"))
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game(name="{} ./[True Edge]").format(pref))
