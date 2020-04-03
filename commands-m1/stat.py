627f23da7cb8c048187fc76fdd3aee91c780404f
DMNight6 <arnokong365@gmail.com>
2020-04-03 09:56:37 +0100

    Rasp Git Commit

--- commands-m1/stat.py
+++ commands-m1/stat.py
@@ -1,4 +1,4 @@
-zz@bot.event
+@bot.event
 async def on_ready():
     print('----------\nLogged As [{0.user}]\nUser ID: [{0.user.id}]\n----------'.format(bot))
-    await bot.change_presence(status=discord.Status.online, activity=discord.Game(name="{} ./[True Edge]").format(pref))
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game(name=f"{pref} ./[True Edge]"))
