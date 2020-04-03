6274ffa49f25484775afe79f996f04c97afd9b7c
DMNight6 <arnokong365@gmail.com>
2020-04-03 11:14:29 +0100

    ?

--- commands-m1/userinfo.py
+++ commands-m1/userinfo.py
@@ -1,8 +1,5 @@
 @bot.command()
 async def ui(ctx, member: discord.Menber):
-
-    roles = [role for role in member.roles]
-
     embed = discord.Embed(title="{} Info", colour=discord.Color.from_rgb(255, 215,0))
 
 
@@ -15,7 +12,6 @@ async def ui(ctx, member: discord.Menber):
     embed.add_field(name=f"Created At", value=member.created_at.strftime("%a, %m %b %d %y, %I:%M %p UTC"))
     embed.add_field(name=f"Joined At", value=member.joined_at.strftime("%a, %m %b %d %y, %I:%M %p UTC"))
 
-    embed.add_field(name=f"Roles ({len(roles)})", value=" ".join([role.mention for role in roles]))
     embed.add_field(name="Bot?", value=member.bot)
 
     await ctx.send(embed=embed)
