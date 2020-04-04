ef89718ea88be74f255cc53d72dc91b25f227a49
DMNight6 <arnokong365@gmail.com>
2020-04-04 22:09:01 +0800

    Update userinfo.py

--- commands-m1/userinfo.py
+++ commands-m1/userinfo.py
@@ -1,7 +1,6 @@
 @bot.command()
-async def ui(ctx, member: discord.Member):
+async def ui(ctx, *,member: discord.Member):
     embed = discord.Embed(title="{} Info", colour=discord.Color.from_rgb(255, 215,0))
-
 
     embed.set_footer(text=f"Requested By: {ctx.author.name}", icon_url=ctx.author.avatar_url)
     embed.set_thumbnail(url=member.avatar_url)
