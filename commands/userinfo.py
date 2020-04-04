b39098ba00b6d8c6aa2415ae773ea3b725818bcd
DMNight6 <arnokong365@gmail.com>
2020-04-04 23:30:45 +0800

    Moved userinfo.py to Actual Cog

---
+++ commands/userinfo.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+
+class UserInfo(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def stat(self, ctx, member: discord.Member):
+        embed = discord.Embed(title=f"{member} Info", colour=discord.Color.from_rgb(255, 215, 0))
+
+        embed.add_field(name=f"Name - {member}")
+        embed.add_field(name=f"In Guild Name - {member.name}")
+        embed.add_field(name="Joined At - ", value=member.joined_at.strftime("%a, %#d %B %Y, %I:%N -> %p UTC"))
+        embed.add_field(name="Created At - ", value=member.created_at.strftime("%a, %#d %B %Y, %I:%N -> %p UTC"))
+        embed.add_field(name="Bot?", value=member.bot)
+        embed.set_thumbnail(member.avatar_url)
+        embed.set_footer(text=f"Requested By > {ctx.author.name}", icon_url=ctx.author.avatar_url)
+
+        await ctx.send(embed=embed)
