0e89b75453511233348762fb58ee09c955988c36
DMNight6 <arnokong365@gmail.com>
2020-04-05 15:50:01 +0800

    Update userinfo.py

--- commands/userinfo.py
+++ commands/userinfo.py
@@ -7,15 +7,15 @@ class UserInfo(commands.Cog):
 
     @commands.command()
     async def stat(self, ctx, member: discord.Member):
-        embed = discord.Embed(title=f"{member} Info", colour=discord.Color.from_rgb(255, 215, 0))
+        embed = discord.Embed(title=f"{member.name} Info", colour=discord.Color.from_rgb(255, 215, 0))
 
         embed.add_field(name="Name - ", value=member)
-        embed.add_field(name="In Guild Name - ", value=member.name)
-        embed.add_field(name="Joined At - ", value=member.joined_at.strftime("%d %b %Y"))
-        embed.add_field(name="Created At - ", value=member.created_at.strftime("%#d %b %Y"))
-        embed.add_field(name="Bot?", value=member.bot)
+        embed.add_field(name="In Guild Name - ", value=member.nick, inline=True)
+        embed.add_field(name="Joined At - ", value=member.joined_at.strftime("%d %b %Y"), inline=True)
+        embed.add_field(name="Created At - ", value=member.created_at.strftime("%#d %b %Y"), inline=True)
+        embed.add_field(name="Bot? - ", value=member.bot, inline=True)
         embed.set_thumbnail(url=member.avatar_url)
-        embed.set_footer(text=f"Requested By > {ctx.author.name}", icon_url=ctx.author.avatar_url)
+        embed.set_footer(text=f"Requested By -> {ctx.author.name}", icon_url=ctx.author.avatar_url)
 
         await ctx.send(embed=embed)
 
