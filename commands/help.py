8c8a06f7d41d253884941af618d5ed999c388815
DMNight6 <arnokong365@gmail.com>
2020-04-06 19:35:11 +0800

    Update help.py

--- commands/help.py
+++ commands/help.py
@@ -6,26 +6,41 @@ class Help(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def help(self, ctx):
+    async def help(self, ctx, args=None):
         embed = discord.Embed(title="List Of Command", colour=discord.Color.from_rgb(255, 215, 0))
+        
+        if args == None:
+            embed.add_field(name="Command", value=f"{pref} command")
+            embed.add_field(name="Mods Command", value=f"{pref} mod")
+            embed.add_field(name="Music Command", value=f"{pref} music")
+            embed.set_footer(text=f"Requested By -> {ctx.author}", icon_url=ctx.author.avatar_url)
 
-        embed.add_field(name="join", value="Join User's Voice Channel", inline=False)
-        embed.add_field(name="leave", value="Clear Music Queues And Leave Voice Channel", inline=False)
-        embed.add_field(name="volume <1-100>", value="Change Bot Music Volume", inline=False)
-        embed.add_field(name="now", value="Shows Current Playing Song", inline=False)
-        embed.add_field(name="pause", value="Pauses Current Playing Song", inline=False)
-        embed.add_field(name="resume", value="Resume Paused Song", inline=False)
-        embed.add_field(name="stop", value="Stop All Songs And Clear Queue")
-        embed.add_field(name="skip", value="Skips Song If User Is Requester, If Not, Start Vote.", inline=False)
-        embed.add_field(name="queue", value="Shows Queue", inline=False)
-        embed.add_field(name="shuffle", value="Shuffles The Queue", inline=False)
-        embed.add_field(name="remove <value>", value="Remove Song From Queue", inline=False)
-        embed.add_field(name="loop", value="Loops Current Songs")
-        embed.add_field(name="play <name/links>", value="Play Song, Will Add Next Requested Music To Queue", inline=False)
-        embed.add_field(name="info", value="Shows Bot Info", inline=False)
-        embed.add_field(name="ping", value="Returns Pong!", inline=False)
-        embed.add_field(name="stat <mention member>", value="Shows A User Info")
+        if args == command:
+            embed.add_field(name="info", value="Shows Bot Info", inline=False)
+            embed.add_field(name="stat <mention member>", value="Shows A User Info")
+            embed.set_footer(text=f"Requested By -> {ctx.author}", icon_url=ctx.author.avatar_url)
+        
+        if args == music:
+            embed.add_field(name="join", value="Join User's Voice Channel", inline=False)
+            embed.add_field(name="leave", value="Clear Music Queues And Leave Voice Channel", inline=False)
+            embed.add_field(name="volume <1-100>", value="Change Bot Music Volume", inline=False)
+            embed.add_field(name="now", value="Shows Current Playing Song", inline=False)
+            embed.add_field(name="pause", value="Pauses Current Playing Song", inline=False)
+            embed.add_field(name="resume", value="Resume Paused Song", inline=False)
+            embed.add_field(name="stop", value="Stop All Songs And Clear Queue")
+            embed.add_field(name="skip", value="Skips Song If User Is Requester, If Not, Start Vote.", inline=False)
+            embed.add_field(name="queue", value="Shows Queue", inline=False)
+            embed.add_field(name="shuffle", value="Shuffles The Queue", inline=False)
+            embed.add_field(name="remove <value>", value="Remove Song From Queue", inline=False)
+            embed.add_field(name="loop", value="Loops Current Songs")
+            embed.add_field(name="play <name/links>", value="Play Song, Will Add Next Requested Music To Queue", inline=False)
+            embed.set_footer(text=f"Requested By -> {ctx.author}", icon_url=ctx.author.avatar_url)
 
+        if args == mod:
+            embed.add_field(name="kick <member> <reason>", value="Kick A Member Out The Guild")
+            embed.add_field(name="ban <member> <reason>", value="Ban a member")
+            embed.add_field(name="clear/purge <amount>", value="Clear Messages With The Amount Provided")
+            embed.set_footer(text=f"Requested By -> {ctx.author}", icon_url=ctx.author.avatar_url)
         await ctx.send(embed=embed)
 
 def setup(bot):
