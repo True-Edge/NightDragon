8ac69a2b56462f7a31f1af6315cdb2f7acc667d4
DMNight6 <arnokong365@gmail.com>
2020-04-07 10:07:00 +0800

    Update greeting.py

--- commands/greeting.py
+++ commands/greeting.py
@@ -10,7 +10,7 @@ class greetCog(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        if member.guild.id == "413632902480396298":
+        if discord.Guild.id == "413632902480396298":
             r = get(member.guild.roles, name='[Not Verified]')
             await member.send("Welcome @{}, Please Do {}verify To Start Verify!".format(member, pref))
             await member.add_roles(r)
