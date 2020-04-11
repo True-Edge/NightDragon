65aeb5ba7c31ffcd5106db90b6de28df46ed6024
DMNight6 <arnokong365@gmail.com>
2020-04-11 18:58:23 +0800

    Update h.py

--- h.py
+++ h.py
@@ -13,10 +13,11 @@ setup_ext_commands(Bot, 'n$')
 
 owner_id = 413632156061925380
 owner_id2 = 300126997718237195
+owner_id3 = 376102007779360769
 
 @Bot.commands
 async def update(client, message):
-    if message.author.id != owner_id and message.author.id != owner_id2:
+    if message.author.id != owner_id and message.author.id != owner_id2 and message.author.id != owner_id3:
         embed = Embed(title="ID Not Match", description=f"```Your ID Does Not Match With The System Provided ID!```", color=Color.from_rgb(255, 215, 0))
         await client.message_create(message.channel, embed=embed)
     else:
