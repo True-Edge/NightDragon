e7a002a6d736c2cbbffe28e4e57851a488222e75
DMNight6 <arnokong365@gmail.com>
2020-04-14 11:57:15 +0800

    Update userinfo.py

--- commands/userinfo.py
+++ commands/userinfo.py
@@ -7,6 +7,7 @@ class UserInfo(commands.Cog):
 
     @commands.command()
     async def stat(self, ctx, member: discord.Member):
+        roles = [role for role in member.roles]
         embed = discord.Embed(title=f"{member.name} Info", colour=discord.Color.from_rgb(255, 215, 0))
 
         embed.add_field(name="Name - ", value=member)
@@ -14,6 +15,7 @@ class UserInfo(commands.Cog):
         embed.add_field(name="Joined At - ", value=member.joined_at.strftime("%d %b %Y"), inline=True)
         embed.add_field(name="Created At - ", value=member.created_at.strftime("%#d %b %Y"), inline=True)
         embed.add_field(name="Bot? - ", value=member.bot, inline=True)
+        embed.add_field(name=f"Roles ({len(roles)})", value=" ".join([role.mention for role in roles]))
         embed.set_thumbnail(url=member.avatar_url)
         embed.set_footer(text=f"Requested By -> {ctx.author.name}", icon_url=ctx.author.avatar_url)
 
