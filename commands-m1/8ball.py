0e122403ea7a30381aae9bf9d428cf99f0f3d1f0
DMNight6 <arnokong365@gmail.com>
2020-04-21 10:51:34 +0800

    Update 8ball.py

--- commands-m1/8ball.py
+++ commands-m1/8ball.py
@@ -1,7 +1,8 @@
 @bot.command()
 async def question(ctx, args):
     A = (["Yes", "No", "I don't think so"])
-    embed = discord.Embed(title="8 Ball Question", colour=discord.Color.from_rgb(215, 255, 0))
-    embed.add_field(name="Question >", value=f"{args}")
-    embed.add_field(name="Answer >", value=random.choice(A))
+    embed = discord.Embed(title="8 Ball Question", colour=discord.Color.from_rgb(255, 215, 0))
+    embed.add_field(name="Question >", value=f"{args}", inline=False)
+    embed.add_field(name="Answer >", value=random.choice(A), inline=False)
+    embed.set_footer(text=f"Requested By > {ctx.author}", icon_url=ctx.author.avatar_url)
     await ctx.send(embed=embed)
