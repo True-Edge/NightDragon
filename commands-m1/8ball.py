7cf50330b652d77fb7b0188acda3e599d6e1924c
DMNight6 <arnokong365@gmail.com>
2020-04-21 10:48:27 +0800

    Update 8ball.py

--- commands-m1/8ball.py
+++ commands-m1/8ball.py
@@ -2,6 +2,6 @@
 async def question(ctx, args):
     A = (["Yes", "No", "I don't think so"])
     embed = discord.Embed(title="8 Ball Question", colour=discord.Color.from_rgb(215, 255, 0))
-        embed.add_field(name="Question >", value=f"{args}")
-        embed.add_field(name="Answer >", value=random.choice(A))
+    embed.add_field(name="Question >", value=f"{args}")
+    embed.add_field(name="Answer >", value=random.choice(A))
     await ctx.send(embed=embed)
