9faa035f5c5d44523973a777d692d05d3cdcab49
DMNight6 <arnokong365@gmail.com>
2020-04-26 18:07:03 +0800

    Revert Fix

---
+++ h.py
@@ -0,0 +1,33 @@
+import hata
+import os
+import dotenv
+from hata.ext.commands import setup_ext_commands
+from hata import Client, start_clients
+from hata import Embed
+from hata import Color
+from dotenv import load_dotenv
+load_dotenv()
+
+Token = os.environ.get("TOKEN")
+Bot = Client(Token)
+setup_ext_commands(Bot, 'n$')
+
+owner_id1 = 413632156061925380
+owner_id2 = 300126997718237195
+owner_id3 = 376102007779360769
+
+@Bot.commands
+async def update(client, message):
+    if message.author.id != owner_id1 and message.author.id != owner_id2 and message.author.id != owner_id3:
+        embed = Embed(title="ID Not Match", description=f"```Your ID Does Not Match With Author ID!```", color=Color.from_rgb(255, 215, 0))
+        await client.message_create(message.channel, embed=embed)
+    else:
+        embed = Embed(title="ID Matched", description="```Bot Updating....```", color=Color.from_rgb(255, 215, 0))
+        await client.message_create(message.channel, embed=embed)
+        os.system('./stop.sh')
+        
+@Bot.events
+async def ready(client):
+    print(f"{client:f} Is Ready!")
+
+start_clients() 
--- start.sh
+++ start.sh
@@ -8,6 +8,6 @@ while :
 do
 git fetch codeberg master; git pull codeberg master
 clear
-bash -c "exec -a pybot python3 d.py"
+bash -c "exec -a pybot python3 d.py" & bash -c "exec -a hata-bot python3 h.py"
 bash stop.sh
 done
