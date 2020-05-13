9c7592360b583bd613114b843502be90098c33a2
DMNight6 <arnokong365@gmail.com>
2020-05-13 20:02:25 +0800

    Fixing some issue

--- commands-m1/update.py
+++ commands-m1/update.py
@@ -3,7 +3,7 @@ async def update(ctx):
     if ctx.author.id in owner_id:
         embed = discord.Embed(title="Access Granted", description="```Updating```", color=discord.Color.from_rgb(255, 215, 0))
         await ctx.send(embed=embed)
-        os.system("stop.sh")
+        os.system("./stop.sh")
     else:
         embed = discord.Embed(title="Access Denied", description=f"{ctx.author} ID is not in database", color=discord.Color.from_rgb(255,215,0))
         await ctx.send(embed=embed)
