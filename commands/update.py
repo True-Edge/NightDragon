cfaba23ecd94013d4223593a30c240ed03fcc6b4
DMNight6 <arnokong365@gmail.com>
2020-05-13 20:52:43 +0800

    Update update.py

--- commands/update.py
+++ commands/update.py
@@ -14,6 +14,7 @@ class Update(commands.Cog):
             embed = discord.Embed(title="Access Granted", description="```Updating```", color=discord.Color.from_rgb(255, 215, 0))
             await ctx.send(embed=embed)
             os.system("./stop.sh")
+            sys.exit()
         else:
             embed = discord.Embed(title="Access Denied", description=f"{ctx.author} ID is not in database", color=discord.Color.from_rgb(255,215,0))
             await ctx.send(embed=embed)
