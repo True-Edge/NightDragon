9d8b4a6d9c454ae5ca8d0ed33bbfa9042e716236
DMNight6 <arnokong365@gmail.com>
2020-05-14 16:45:36 +0800

    Update update.py

--- commands-m1/update.py
+++ commands-m1/update.py
@@ -1,5 +1,5 @@
 @bot.command()
-async def update(self, ctx):
+async def update(ctx):
     if ctx.author.id in owner_id:
         embed = discord.Embed(title="Access Granted", description="Restarting Service ``TMUX``", color=discord.Color.from_rgb(255, 215, 0))
         await ctx.send(embed=embed)
