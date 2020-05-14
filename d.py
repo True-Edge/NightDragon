1501b3e41de9e3e20f27ab70483818ee7e11be99
DMNight6 <arnokong365@gmail.com>
2020-05-14 12:55:38 +0800

    Added presence loop

--- d.py
+++ d.py
@@ -9,6 +9,7 @@ import youtube_dl
 
 import asyncio
 import datetime
+import time
 import sys
 
 import os
@@ -17,18 +18,24 @@ import random
 from os import system
 
 pref = open("System/prefix.u.g", "r").read()
+status = [f"{pref} ./[True Edge]", "wollycraft.net"]
 owner_id = 413632156061925380, 376102007779360769, 300126997718237195, 529290942306320384, 615942459943288843
 guild_id = "413632902480396298"
 load_dotenv()
 token = os.getenv("TOKEN")
 
-bot = commands.Bot(command_prefix = pref, case_insensitive=True, status=discord.Status.idle, activity=discord.Game(name="Loading..."))
+bot = commands.Bot(command_prefix = pref, case_insensitive=True, status=discord.Status.idle, activity=discord.Activity(type=2, name="System Mainframe"))
 bot.remove_command('help')
 
+async def change_stat():
+    await bot.wait_until_ready()
+    while not bot.is_closed():
+        st = random.choice(status)
+        await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=3, name=(st)))
+        await asyncio.sleep(10)
 @bot.event
 async def on_ready():
     print('----------\nLogged As [{0.user}]\nUser ID: [{0.user.id}]\n----------'.format(bot))
-    await bot.change_presence(status=discord.Status.online, activity=discord.Game(name=f"{pref} ./[True Edge]"))
 
 for cog in os.listdir('commands'):
     if cog.endswith(".py"):
@@ -45,4 +52,5 @@ for extra_command in os.listdir('commands-m1'):
             with open(f'commands-m1/{extra_command}') as rk:
                 exec(rk.read())
 
+bot.loop.create_task(change_stat())
 bot.run(token)
