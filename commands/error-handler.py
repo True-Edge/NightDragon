048950007ea6aff400f77d150608040c4939c6bd
DMNight6 <arnokong365@gmail.com>
2020-05-28 22:57:43 +0800

    Update error-handler.py

--- commands/error-handler.py
+++ commands/error-handler.py
@@ -1,5 +1,5 @@
 @bot.event
 async def on_command_error(ctx, error):
-    embed = discord.Embed(title="Unknown Command!", description=f"Do {pref}help command\music",color=discord.Color.from_rgb(255, 215, 0))
+    embed = discord.Embed(title="Unknown Command!", description=f"Invalid command provided",color=discord.Color.from_rgb(255, 215, 0))
     embed.set_footer(text=f"Attempted By -> {ctx.author}", icon_url=ctx.author.avatar_url)
     await ctx.send(embed=embed)
