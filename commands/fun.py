831a573d22e8bc3bd31deb6df13dd3ec3e8fba57
DMNight6 <arnokong365@gmail.com>
2020-06-03 13:08:03 +0800

    Update fun.py

--- commands/fun.py
+++ commands/fun.py
@@ -1,4 +1,4 @@
 @bot.command(description="Random command")
-async def slap(ctx, member=discord.Member):
+async def slap(ctx, member=bot.guilds):
     a = random.choice(member)
     await ctx.send(f"{ctx.author} Slapped {a}")
