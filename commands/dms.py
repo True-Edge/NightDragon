214e2e61112ac26cd6b6e4c22bdecda2c4c85dd4
DMNight6 <arnokong365@gmail.com>
2020-08-19 22:02:17 +0800

    Removed Unneeded Command

--- commands/dms.py
+++
@@ -1,6 +0,0 @@
-@bot.command()
-@commands.has_role("Staff")
-async def dm(ctx, member: discord.Member, *,reason="None"):
-    await ctx.message.delete()
-    await member.send(reason)
-    await ctx.send("Message Sended")
