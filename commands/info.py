1ba296633e3b89fc3131769dd5d04b336b04b6be
DMNight6 <arnokong365@gmail.com>
2020-08-19 22:07:38 +0800

    Update info.py

--- commands/info.py
+++ commands/info.py
@@ -1,12 +1,11 @@
 @bot.command()
 async def info(ctx):
     await ctx.message.delete()
-    urli = open('System/Invite-Link', 'r').read()
+
     embed1 = discord.Embed(
         title='About Me',
         colour=discord.Color.from_rgb(215, 255, 0),
-        description="Created At: 14th March 2019\n[Discord Server](https://discord.gg/tehurtf)\n[Invite Me!]({})".format(urli)
-        )
+        description="Created At: 14th March 2019\n[Discord Server](discord.gg/GhPyRgx)\n[Invite Me!](https://discordapp.com/api/oauth2/authorize?client_id=648085129599647744&permissions=2147483127&scope=bot)")
 
     await ctx.send(embed=embed1)        
         
